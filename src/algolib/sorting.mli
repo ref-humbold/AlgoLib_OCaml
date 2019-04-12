1d321e94cffb987ac4dfe16af9f44e6d84521b79
R. Kaleta <rafkaleta@onet.pl>
2019-04-12 10:40:07 +0200

    Doccomments for sorting

--- src/algolib/sorting.mli
+++ src/algolib/sorting.mli
@@ -1,8 +1,9 @@
-(* SORTING ALGORITHMS *)
-type point = float * float;;
+(** SORTING ALGORITHMS *)
 
-(** [angle_sort lst] sorts points [lst] on a plane with respect to the angle. *)
+type point = float * float
+
 val angle_sort: point list -> point list
+(** [angle_sort lst] sorts points [lst] on a plane with respect to the angle *)
 
-(** [merge_sort lst] sorts sequence [lst] using merge sort algorithm. *)
 val merge_sort: 'a list -> 'a list
+(** [merge_sort lst] sorts sequence [lst] using merge sort algorithm *)
