8394ca4ea5848bd3e1e30a0fbcf141409253c79a
R. Kaleta <rafkaleta@onet.pl>
2019-04-12 11:14:58 +0200

    Doccomments for indexed list

--- src/algolib/structures/indexed_list.mli
+++ src/algolib/structures/indexed_list.mli
@@ -1,30 +1,31 @@
-(* INDEXED LIST STRUCTURE *)
-(** The type of indexed lists. *)
+(** Indexed list structure *)
+
 type 'a t
+(** The type of indexed lists *)
 
-(** Exception raised when retrieving or removing elements from empty indexed lists. *)
 exception EmptyList
+(** Exception raised when retrieving or removing elements from empty indexed lists *)
 
-(** Exception raised when index is out of range. *)
 exception InvalidIndex
+(** Exception raised when index is out of range *)
 
-(** [create ()] returns a new indexed list. *)
 val create: unit -> 'a t
+(** [create ()] returns a new indexed list *)
 
-(** [is_empty lst] checks if indexed list [lst] is empty. *)
 val is_empty: 'a t -> bool
+(** [is_empty lst] checks if indexed list [lst] is empty *)
 
-(** [head lst] retrieves the first element of indexed list [lst]. *)
 val head: 'a t -> 'a
+(** [head lst] retrieves the first element of indexed list [lst] *)
 
-(** [cons x lst] adds element [x] at the front of indexed list [lst]. *)
 val cons: 'a -> 'a t -> 'a t
+(** [cons x lst] adds element [x] at the front of indexed list [lst] *)
 
-(** [tail lst] returns indexed list without first element of indexed list [lst]. *)
 val tail: 'a t -> 'a t
+(** [tail lst] returns indexed list without first element of indexed list [lst] *)
 
-(** [elem n lst] retrieves element indexed with [n] in indexed list [lst]. *)
 val elem: int -> 'a t -> 'a
+(** [elem n lst] retrieves element indexed with [n] in indexed list [lst] *)
 
-(** [update n x lst] sets [x] as element indexed with [n] in indexed list [lst]. *)
 val update: int -> 'a -> 'a t -> 'a t
+(** [update n x lst] sets [x] as element indexed with [n] in indexed list [lst] *)
