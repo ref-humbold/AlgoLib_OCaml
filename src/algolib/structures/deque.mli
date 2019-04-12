a06e8f7f31b548bb9c91af53813d81d1ad45d4ec
R. Kaleta <rafkaleta@onet.pl>
2019-04-12 10:54:41 +0200

    Doccomments for deque

--- src/algolib/structures/deque.mli
+++ src/algolib/structures/deque.mli
@@ -1,30 +1,30 @@
-(* DOUBLE-ENDED QUEUE STRUCTURE *)
-(** The type of deques containing elements of type 'a. *)
+(** Double-ended queue structure *)
 type 'a t
+(** The type of deques containing elements of type 'a *)
 
-(** Exception raised when retrieving or removing elements from empty deque. *)
 exception EmptyDeque
+(** Exception raised when retrieving or removing elements from empty deque *)
 
-(** [create ()] returns a new deque. *)
 val create: unit -> 'a t
+(** [create ()] returns a new deque *)
 
-(** [is_empty d] checks if deque [d] is empty. *)
 val is_empty: 'a t -> bool
+(** [is_empty d] checks if deque [d] is empty *)
 
-(** [front d] retrieves element from the front of deque [d]. *)
 val front: 'a t -> 'a
+(** [front d] retrieves element from the front of deque [d] *)
 
-(** [back d] retrieves element from the back of deque [d]. *)
 val back: 'a t -> 'a
+(** [back d] retrieves element from the back of deque [d] *)
 
-(** [push_front x d] adds element [x] at the front of deque [d]. *)
 val push_front: 'a -> 'a t -> 'a t
+(** [push_front x d] adds element [x] at the front of deque [d] *)
 
-(** [push_back x d] adds element [x] at the back of deque [d]. *)
 val push_back: 'a -> 'a t -> 'a t
+(** [push_back x d] adds element [x] at the back of deque [d] *)
 
-(** [pop_front d] removes element from the front element of deque [d]. *)
 val pop_front: 'a t -> 'a t
+(** [pop_front d] removes element from the front element of deque [d] *)
 
-(** [pop_back d] removes element from the back of deque [d]. *)
 val pop_back: 'a t -> 'a t
+(** [pop_back d] removes element from the back of deque [d] *)
