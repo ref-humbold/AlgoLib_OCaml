a5a944bdf1cfd3d481a214f65e9d90c5389f92de
R. Kaleta <rafkaleta@onet.pl>
2019-04-12 11:35:20 +0200

    Update hm_queue.ml

--- src/algolib/structures/hm_queue.ml
+++ src/algolib/structures/hm_queue.ml
@@ -1,4 +1,4 @@
-(* REAL-TIME HOOD-MELVILLE QUEUE STRUCTURE *)
+(* Real-time Hood-Melville queue structure *)
 type 'a combining = Rev of int * 'a list * 'a list * 'a list * 'a list
                   | Apd of int * 'a list * 'a list
                   | End of 'a list
@@ -16,35 +16,35 @@ let is_empty (flen, ft, _, _, bk) =
 
 let front (_, ft, _, _, _) =
   match ft with
-  | x::_ -> x
+  | x :: _ -> x
   | [] -> raise EmptyQueue
 
 let rebalance_ ((flen, ft, _, blen, bk) as q) =
   let comb rs =
     match rs with
-    | Rev (len, x::ft_, rft, y::bk_, rbk) -> Rev (len, ft_, x::rft, bk_, y::rbk)
-    | Rev (len, [], rft, [y], rbk) -> Apd (len, rft, y::rbk)
-    | Apd (0, _, bk_) -> End bk_
-    | Apd (len, x::ft_, bk_) -> Apd (len, ft_, x::bk_)
+    | Rev (len, x :: ft', rft, y :: bk', rbk) -> Rev (len, ft', x :: rft, bk', y :: rbk)
+    | Rev (len, [], ft', [y], bk') -> Apd (len, ft', y :: bk')
+    | Apd (0, _, bk') -> End bk'
+    | Apd (len, x :: ft', bk') -> Apd (len, ft', x :: bk')
     | Rev _ | Apd _ | End _ | Null -> rs in
-  let combine (flen_, ft_, rs, blen_, bk_) =
-    match comb (comb rs) with
-    | End nft -> (flen_, nft, Null, blen_, bk_)
-    | (Rev _ as nrs) | (Apd _ as nrs) | (Null as nrs) ->
-      (flen_, ft_, nrs, blen_, bk_) in
+  let combine (flen', ft', rs', blen', bk') =
+    match comb (comb rs') with
+    | End ft'' -> (flen', ft'', Null, blen', bk')
+    | (Rev _ as rs'') | (Apd _ as rs'') | (Null as rs'') ->
+      (flen', ft', rs'', blen', bk') in
   if blen <= flen
   then combine q
   else let rev = Rev (0, ft, [], bk, []) in combine (flen + blen, ft, rev, 0, [])
 
-let push x (flen, ft, rs, blen, bk) = rebalance_ (flen, ft, rs, blen + 1, x::bk)
+let push x (flen, ft, rs, blen, bk) = rebalance_ (flen, ft, rs, blen + 1, x :: bk)
 
 let pop (flen, ft, rs, blen, bk) =
   let decrement rs =
     match rs with
     | Rev (len, ft, rft, bk, rbk) -> Rev (len - 1, ft, rft, bk, rbk)
-    | Apd (0, _, _::bk) -> End bk
-    | Apd (len, ft, bk) -> Apd (len -1, ft, bk)
+    | Apd (0, _, _ :: bk) -> End bk
+    | Apd (len, ft, bk) -> Apd (len - 1, ft, bk)
     | End _ | Null -> rs in
   match ft with
-  | _::xs -> rebalance_ (flen - 1, xs, decrement rs, blen, bk)
+  | _ :: xs -> rebalance_ (flen - 1, xs, decrement rs, blen, bk)
   | [] -> raise EmptyQueue
