d57b1bffef9a5ff182e0618a98cc812cfeaea92f
R. Kaleta <rafkaleta@onet.pl>
2019-04-12 10:49:05 +0200

    Doccomments for maths

--- src/algolib/math/maths.mli
+++ src/algolib/math/maths.mli
@@ -1,13 +1,13 @@
-(* MATHS ALGORITHMS *)
+(** Mathematics algorithms *)
 
-(** [gcdiv n1 n2] computes the greatest common divisor of given two non-negative integers [n1] and [n2]. *)
 val gcdiv: int -> int -> int
+(** [gcdiv n1 n2] computes the greatest common divisor of two non-negative integers [n1] and [n2] *)
 
-(** [lcm n1 n2] computes the least common multiple of given two non-negative integers [n1] and [n2]. *)
 val lcm: int -> int -> int
+(** [lcm n1 n2] computes the least common multiple of two non-negative integers [n1] and [n2] *)
 
-(** [mult_mod f1 f2 m] performs fast integer multiplication of [f1] and [f2] taken modulo [m]. *)
 val mult_mod: int -> int -> int -> int
+(** [mult_mod f1 f2 m] performs fast integer multiplication of [f1] and [f2] taken modulo [m] *)
 
-(** [power_mod b e m] performs fast integer exponentiation as [b] to the power of [e] taken modulo [m]. *)
 val power_mod: int -> int -> int -> int
+(** [power_mod b e m] performs fast integer exponentiation of [b] to the power of [e] taken modulo [m] *)
