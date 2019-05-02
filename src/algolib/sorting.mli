9e46add873230f62acf25e8a7cfeb68c90b8df94
R. Kaleta <rafkaleta@onet.pl>
2019-05-02 19:31:53 +0200

    Extracted sorting of points

--- src/algolib/sorting.mli
+++ src/algolib/sorting.mli
@@ -1,9 +1,4 @@
-(** Sorting algorithms. *)
-
-type point = float * float
-
-val angle_sort: point list -> point list
-(** [angle_sort lst] sorts points [lst] on a plane with respect to the angle. *)
+(** List sorting algorithms. *)
 
 val merge_sort: ('a -> 'a -> bool) -> 'a list -> 'a list
 (** [merge_sort cmp lst] sorts list [lst] according to comparison function [cmp] using merge sort algorithm. *)
