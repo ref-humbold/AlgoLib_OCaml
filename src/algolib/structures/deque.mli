ba9fc27456ebe4245208094a15e386c5c07aea95
R. Kaleta <rafkaleta@onet.pl>
2019-05-15 11:59:50 +0200

    Update deque.mli

--- src/algolib/structures/deque.mli
+++ src/algolib/structures/deque.mli
@@ -24,13 +24,13 @@ val push_front : 'a -> 'a t -> 'a t
 (** [push_front x d] adds element [x] at the front of deque [d]. *)
 
 val (@++) : 'a -> 'a t -> 'a t
-(** [(@++)] is an alias of [push_front]. Right associative. *)
+(** [(@++)] is an infix alias of [push_front]. Right associative. *)
 
 val push_back : 'a t -> 'a -> 'a t
 (** [push_back d x] adds element [x] at the back of deque [d]. *)
 
 val (&++) : 'a t -> 'a -> 'a t
-(** [(&++)] is an alias of [push_back]. Left associative. *)
+(** [(&++)] is an infix alias of [push_back]. Left associative. *)
 
 val pop_front : 'a t -> 'a t
 (** [pop_front d] removes element from the front element of deque [d].
