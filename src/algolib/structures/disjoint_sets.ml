5d8fdbb728116bc4abb6f9a60970fc27aedeb044
R. Kaleta <rafkaleta@onet.pl>
2020-02-15 01:45:31 +0100

    New functions in disjoint sets

--- src/algolib/structures/disjoint_sets.ml
+++ src/algolib/structures/disjoint_sets.ml
@@ -10,15 +10,21 @@ module type DISJOINT_SETS = sig
 
   type t
 
+  exception ElementPresent of elem
+
   val create : unit -> t
 
   val size : t -> int
 
   val contains : elem -> t -> bool
 
-  val add_elem : elem -> t -> unit
+  val add_list : elem list -> t -> unit
+
+  val add_seq : elem Seq.t -> t -> unit
 
   val find_set : elem -> t -> elem
+
+  val find_set_or_default : elem -> elem -> t -> elem
 
   val is_same_set : elem -> elem -> t -> bool
 
@@ -32,17 +38,23 @@ module Make (Cmp : COMPARABLE) : DISJOINT_SETS with type elem = Cmp.t = struct
 
   type t = {mutable size : int; mutable map : elem Repr.t}
 
+  exception ElementPresent of elem
+
   let create () : t = {size = 0; map = Repr.empty}
 
   let size {size; _} = size
 
   let contains element {map; _} = Repr.mem element map
 
-  let add_elem element dset =
-    if not @@ Repr.mem element dset.map
-    then (
-      dset.size <- dset.size + 1 ;
-      dset.map <- Repr.add element element dset.map )
+  let add_list elements dset =
+    List.iter (fun e -> if Repr.mem e dset.map then raise @@ ElementPresent e) elements ;
+    List.iter
+      (fun e ->
+         dset.map <- Repr.add e e dset.map ;
+         dset.size <- dset.size + 1)
+      elements
+
+  let add_seq elements dset = add_list (List.of_seq elements) dset
 
   let rec find_set element dset =
     let value = Repr.find element dset.map in
@@ -52,6 +64,10 @@ module Make (Cmp : COMPARABLE) : DISJOINT_SETS with type elem = Cmp.t = struct
       let repr = find_set value dset in
       dset.map <- Repr.add element repr dset.map ;
       repr
+
+  let find_set_or_default element default dset =
+    try find_set element dset with
+    | Not_found -> default
 
   let is_same_set element1 element2 dset =
     let repr1 = find_set element1 dset and repr2 = find_set element2 dset in
--- src/algolib/structures/disjoint_sets.mli
+++ src/algolib/structures/disjoint_sets.mli
@@ -1,4 +1,4 @@
-(** Disjoint sets structure (union-find). *)
+(** Disjoint sets structure (union-find) *)
 
 module type COMPARABLE = sig
   type t
@@ -13,6 +13,9 @@ module type DISJOINT_SETS = sig
   type t
   (** The type of sets structures. *)
 
+  exception ElementPresent of elem
+  (** Exception raised when adding already present element. *)
+
   val create : unit -> t
   (** [create ()] produces empty sets structure. *)
 
@@ -22,17 +25,27 @@ module type DISJOINT_SETS = sig
   val contains : elem -> t -> bool
   (** [contains x s] checks if [x] belongs to any set in structure [s]. *)
 
-  val add_elem : elem -> t -> unit
-  (** [add_elem x s] adds new singleton set with [x] to structure [s]. *)
+  val add_list : elem list -> t -> unit
+  (** [add_list xs s] adds new singleton sets from list [xs] to structure [s]. *)
+
+  val add_seq : elem Seq.t -> t -> unit
+  (** [add_seq xs s] adds new singleton sets from Seq [xs] to structure [s]. *)
 
   val find_set : elem -> t -> elem
-  (** [find_set x s] finds the representant of [x] in structure [s]. *)
+  (** [find_set x s] finds the representant of [x] in structure [s]. Raises [Not_found] when [x] is
+      not present in the structure. *)
+
+  val find_set_or_default : elem -> elem -> t -> elem
+  (** [find_set_or_default x d s] finds the representant of [x] in structure [s] if it exists,
+      otherwise returns [d]. *)
 
   val is_same_set : elem -> elem -> t -> bool
-  (** [is_same_set x y s] checks if [x] and [y] belong to the same set in structure [s]. *)
+  (** [is_same_set x y s] checks if [x] and [y] belong to the same set in structure [s]. Raises
+      [Not_found] when neither [x] nor [y] is present in the structure. *)
 
   val union_set : elem -> elem -> t -> unit
-  (** [union_set x y s] joins sets containing [x] and [y] in structure [s]. *)
+  (** [union_set x y s] joins sets containing [x] and [y] in structure [s]. Raises [Not_found] when
+      neither [x] nor [y] is present in the structure. *)
 end
 
 module Make (Cmp : COMPARABLE) : DISJOINT_SETS with type elem = Cmp.t
