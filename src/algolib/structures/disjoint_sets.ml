0361e97f1eddd89364f80f593e0e7ea24290063a
R. Kaleta <rafkaleta@onet.pl>
2020-05-13 00:47:39 +0200

    Use Hashtbl in disjoint sets

--- src/algolib/structures/disjoint_sets.ml
+++ src/algolib/structures/disjoint_sets.ml
@@ -38,42 +38,39 @@ end
 module Make (Cmp : COMPARABLE) : DISJOINT_SETS with type elem = Cmp.t = struct
   type elem = Cmp.t
 
-  module Repr = Map.Make (Cmp)
-
-  type t = {mutable size : int; mutable map : elem Repr.t}
+  type t = {mutable size : int; map : (elem, elem) Hashtbl.t}
 
   exception ElementPresent of elem
 
-  let create () : t = {size = 0; map = Repr.empty}
+  let create () : t = {size = 0; map = Hashtbl.create 37}
 
   let size {size; _} = size
 
-  let contains element {map; _} = Repr.mem element map
+  let contains element {map; _} = Hashtbl.mem map element
 
   let add_seq elements dset =
-    Seq.iter (fun e -> if Repr.mem e dset.map then raise @@ ElementPresent e) elements ;
+    Seq.iter (fun e -> if Hashtbl.mem dset.map e then raise @@ ElementPresent e) elements ;
     Seq.iter
       (fun e ->
-         dset.map <- Repr.add e e dset.map ;
+         Hashtbl.add dset.map e e ;
          dset.size <- dset.size + 1)
       elements
 
   let add_list elements dset =
-    List.iter (fun e -> if Repr.mem e dset.map then raise @@ ElementPresent e) elements ;
+    List.iter (fun e -> if Hashtbl.mem dset.map e then raise @@ ElementPresent e) elements ;
     List.iter
       (fun e ->
-         dset.map <- Repr.add e e dset.map ;
+         Hashtbl.add dset.map e e ;
          dset.size <- dset.size + 1)
       elements
 
   let rec find_set element dset =
-    let value = Repr.find element dset.map in
+    let value = Hashtbl.find dset.map element in
     if Cmp.compare value element = 0
     then element
     else
       let repr = find_set value dset in
-      dset.map <- Repr.add element repr dset.map ;
-      repr
+      Hashtbl.add dset.map element repr ; repr
 
   let find_set_opt element dset =
     try Some (find_set element dset) with
@@ -88,13 +85,13 @@ module Make (Cmp : COMPARABLE) : DISJOINT_SETS with type elem = Cmp.t = struct
     if not @@ is_same_set repr1 repr2 dset
     then (
       dset.size <- dset.size - 1 ;
-      dset.map <- Repr.add repr1 repr2 dset.map )
+      Hashtbl.add dset.map repr1 repr2 )
 
   let of_seq elements =
     { size = Seq.fold_left (fun acc _ -> acc + 1) 0 elements;
-      map = Repr.of_seq @@ Seq.map (fun x -> (x, x)) elements }
+      map = Hashtbl.of_seq @@ Seq.map (fun x -> (x, x)) elements }
 
   let of_list elements =
     { size = List.length elements;
-      map = Repr.of_seq @@ Seq.map (fun x -> (x, x)) @@ List.to_seq elements }
+      map = Hashtbl.of_seq @@ Seq.map (fun x -> (x, x)) @@ List.to_seq elements }
 end
