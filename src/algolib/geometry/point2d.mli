79dc9251c901c3e94dd95f54518a732356f38052
R. Kaleta <rafkaleta@onet.pl>
2020-09-29 22:20:54 +0200

    Vectors in 2D and 3D

--- src/algolib/geometry/point2d.mli
+++ src/algolib/geometry/point2d.mli
@@ -4,17 +4,17 @@
 type point2d = Pt2D of float * float
 
 val pt2d : float -> float -> point2d
-(** [p2d x y] creates a point [(x, y)]. *)
+(** [pt2d x y] creates a point [(x, y)]. *)
 
 val pt2d_i : int -> int -> point2d
-(** [p2d_i x y] creates a point [(x, y)]. *)
+(** [pt2d_i x y] creates a point [(x, y)]. *)
 
 val angle_rad : point2d -> float
-(** [angle p] computes angle in radians of point [p] between its radius and X axis. Value of the
+(** [angle_rad p] computes angle in radians of point [p] between its radius and X axis. Value of the
     angle is between -PI exclusive and PI inclusive. *)
 
 val angle_deg : point2d -> float
-(** [angle p] computes angle in degrees of point [p] between its radius and X axis. Value of the
+(** [angle_deg p] computes angle in degrees of point [p] between its radius and X axis. Value of the
     angle is between 0 inclusive and 360 exclusive. *)
 
 val radius : point2d -> float
---
+++ src/algolib/geometry/vector3d.mli
@@ -0,0 +1,37 @@
+(** Structure of vector in a space *)
+
+(** The type of vectors in a space. *)
+type vector3d = Vec3D of float * float * float
+
+val vec3d : float -> float -> float -> vector3d
+(** [vec3d x y z] creates a vector [\[x, y, z\]]. *)
+
+val vec3d_i : int -> int -> int -> vector3d
+(** [vec3d_i x y z] creates a vector [\[x, y, z\]]. *)
+
+val length : vector3d -> float
+(** [length v] computes length of vector [v]. *)
+
+val ( +$ ) : vector3d -> vector3d -> vector3d
+(** [v1 +$ v2] adds vectors [v1] and [v2]. Left associative. *)
+
+val ( -$ ) : vector3d -> vector3d -> vector3d
+(** [v1 -$ v2] subtracts vectors [v1] and [v2]. Left associative. *)
+
+val ( *$ ) : vector3d -> float -> vector3d
+(** [v *$ c] multiplies each coefficient of vector [v] by a constant [c]. Left associative. *)
+
+val ( /$ ) : vector3d -> float -> vector3d
+(** [v1 /$ v2] divides each coefficient of vector [v] by a constant [c]. Left associative. *)
+
+val dot : vector3d -> vector3d -> float
+(** [dot v1 v2] counts the dot product of vectors [v1] and [v2]. *)
+
+val cross : vector3d -> vector3d -> float
+(** [cross v1 v2] counts the cross product of vectors [v1] and [v2]. *)
+
+val area : vector3d -> vector3d -> float
+(** [area v1 v2] counts the area of parallelogram determined by vectors [v1] and [v2]. *)
+
+val volume : vector3d -> vector3d -> vector3d -> float
+(** [volume v1 v2 v3] counts the volume of parallelepiped determined by vectors [v1], [v2] and [v3]. *)
