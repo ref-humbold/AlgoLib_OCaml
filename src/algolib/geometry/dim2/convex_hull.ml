1ea7f034edb0ca663aae31736b9cbf5aa5d8654c
R. Kaleta <rafkaleta@onet.pl>
2021-08-22 02:10:50 +0200

    Tests for convex hull

--- src/algolib/geometry/dim2/convex_hull.ml
+++ src/algolib/geometry/dim2/convex_hull.ml
@@ -1,27 +1,19 @@
 (* Algorithm for convex hull in 2D (monotone chain) *)
 
 let find_convex_hull (points : Point_2d.point2d list) =
-  let cross_product
-      (Point_2d.Point2D (p1X, p1Y)) (Point_2d.Point2D (p2X, p2Y)) (Point_2d.Point2D (p3X, p3Y))
-    =
-    ((p1X -. p2X) *. (p3Y -. p2Y)) -. ((p3X -. p2X) *. (p1Y -. p2Y))
-  in
+  let cross_product p1 p2 p3 = Vector_2d.area (Vector_2d.between p2 p1) (Vector_2d.between p2 p3) in
   let rec half_hull pts acc =
     match (pts, acc) with
-    | p :: ps, h1 :: h2 :: _ ->
-      if cross_product h2 h1 p <= 0.0 then half_hull ps (List.tl acc) else half_hull ps (p :: acc)
-    | p :: ps, _ -> half_hull ps (p :: acc)
+    | p :: ps, h1 :: (h2 :: _ as htl) ->
+      if cross_product h2 h1 p >= 0.0 then half_hull pts htl else half_hull ps @@ p :: acc
+    | p :: ps, _ -> half_hull ps @@ p :: acc
     | [], acc -> acc
   in
   let sorted = Geometry_2d.sort_by_x points in
   let rsorted = List.rev sorted in
   match (sorted, rsorted) with
-  | p1 :: p2 :: ps, r1 :: r2 :: rs ->
-    let upper = half_hull ps [p2; p1] and lower = half_hull rs [r2; r1] in
-    let rec combine lst1 lst2 acc =
-      match lst2 with
-      | x :: xs -> combine lst1 xs (x :: acc)
-      | [] -> List.rev_append lst1 acc
-    in
-    combine (List.tl upper) (List.tl lower) []
-  | _ -> points
+  | p1 :: p2 :: (_ :: _ as ps), r1 :: r2 :: (_ :: _ as rs) ->
+    let lower = half_hull ps [p2; p1] and upper = half_hull rs [r2; r1] in
+    let merge lst1 lst2 = List.rev_append lst1 @@ List.rev lst2 in
+    merge (List.tl lower) (List.tl upper)
+  | _ -> []
---
+++ test/convex_hull_test.ml
@@ -0,0 +1,88 @@
+(* Tests: Algorithm for convex hull (monotone chain) in 2D *)
+open OUnit2
+open Algolib
+open Algolib.Convex_hull
+open Utils
+
+let print_point (Point_2d.Point2D (x, y)) =
+  "Point2D(" ^ string_of_float x ^ ", " ^ string_of_float y ^ ")"
+
+(* find_convex_hull_Test_list *)
+
+let find_convex_hull__when_one_point__then_empty =
+  "find_convex_hull When one point Then empty"
+  >:: fun _ ->
+    (* when *)
+    let result = find_convex_hull [Point_2d.Point2D (3.0, 2.0)] in
+    (* then *)
+    assert_equal ~printer:(Printers.list print_point) [] result
+
+let find_convex_hull__when_two_points__then_empty =
+  "find_convex_hull When two points Then empty"
+  >:: fun _ ->
+    (* when *)
+    let result = find_convex_hull [Point_2d.Point2D (2.0, 3.0); Point_2d.Point2D (3.0, 2.0)] in
+    (* then *)
+    assert_equal ~printer:(Printers.list print_point) [] result
+
+let find_convex_hull__when_three_points__then_these_points_are_hull =
+  "find_convex_hull When three points Then these points are hull"
+  >:: fun _ ->
+    (* given *)
+    let points =
+      [Point_2d.Point2D (1.0, -1.0); Point_2d.Point2D (5.0, 1.0); Point_2d.Point2D (3.0, 4.0)]
+    in
+    (* when *)
+    let result = find_convex_hull points in
+    (* then *)
+    assert_equal ~printer:(Printers.list print_point) points result
+
+let find_convex_hull__then_points_in_hull =
+  "find_convex_hull Then points in hull"
+  >:: fun _ ->
+    (* when *)
+    let result =
+      find_convex_hull
+        [ Point_2d.pt2d_i 1 (-3); Point_2d.pt2d_i (-4) 6; Point_2d.pt2d_i (-5) (-7);
+          Point_2d.pt2d_i (-8) (-7); Point_2d.pt2d_i (-3) (-4); Point_2d.pt2d_i 5 9;
+          Point_2d.pt2d_i (-1) (-8); Point_2d.pt2d_i (-5) 10; Point_2d.pt2d_i 8 0;
+          Point_2d.pt2d_i 3 (-6); Point_2d.pt2d_i (-2) 1; Point_2d.pt2d_i (-2) 8;
+          Point_2d.pt2d_i 10 2; Point_2d.pt2d_i 6 3; Point_2d.pt2d_i (-7) 7; Point_2d.pt2d_i 6 (-4) ]
+    in
+    (* then *)
+    assert_equal
+      ~printer:(Printers.list print_point)
+      [ Point_2d.pt2d_i (-8) (-7); Point_2d.pt2d_i (-1) (-8); Point_2d.pt2d_i 3 (-6);
+        Point_2d.pt2d_i 6 (-4); Point_2d.pt2d_i 10 2; Point_2d.pt2d_i 5 9; Point_2d.pt2d_i (-5) 10;
+        Point_2d.pt2d_i (-7) 7 ]
+      result
+
+let find_convex_hull__when_multiple_points_are_collinear__then_inner_points_omitted =
+  "find_convex_hull When multiple points are collinear Then inner points omitted"
+  >:: fun _ ->
+    (* when *)
+    let result =
+      find_convex_hull
+        [ Point_2d.pt2d_i (-1) (-3); Point_2d.pt2d_i (-3) (-3); Point_2d.pt2d_i (-3) (-1);
+          Point_2d.pt2d_i 2 (-3); Point_2d.pt2d_i (-3) 5; Point_2d.pt2d_i 0 (-3);
+          Point_2d.pt2d_i 7 (-3); Point_2d.pt2d_i (-3) (-2) ]
+    in
+    (* then *)
+    assert_equal
+      ~printer:(Printers.list print_point)
+      [Point_2d.pt2d_i (-3) (-3); Point_2d.pt2d_i 7 (-3); Point_2d.pt2d_i (-3) 5]
+      result
+
+let find_convex_hull_Test_list =
+  test_list
+    [ find_convex_hull__when_one_point__then_empty; find_convex_hull__when_two_points__then_empty;
+      find_convex_hull__when_three_points__then_these_points_are_hull;
+      find_convex_hull__then_points_in_hull;
+      find_convex_hull__when_multiple_points_are_collinear__then_inner_points_omitted ]
+
+(* find_convex_hull_Test *)
+
+let find_convex_hull_Test =
+  "Tests: Algorithm for convex hull (monotone chain) in 2D" >::: [find_convex_hull_Test_list]
+
+let _ = run_test_tt_main find_convex_hull_Test
